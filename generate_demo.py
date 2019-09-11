9d773ee4213713b37c11e4c79a74b7bdac799cc4
Tobias <thaegi@gmx.de>
2019-09-11 18:33:37 +0200

    generate smaller demo gifs

---
+++ generate_demo.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+import json
+
+import slideshow.cli as cli
+from slideshow.SlideManager import SlideManager
+
+import pkgutil
+
+if __name__ == "__main__":
+
+    config = {}
+    with open(os.path.dirname(os.path.realpath(__file__))+'/config.json') as config_file:
+        config = json.load(config_file)    
+    
+    config.update({
+        "output_width": 400,
+        "output_height": 250,
+        "output_codec": "",
+        "output_parameters": "",
+        "slide_duration": 2,
+        "slide_duration_min": 1,
+        "fade_duration": 1.0,
+        "transition_bars_count": 10,
+        "transition_cell_size": 50,
+        "fps": 30,
+        "overwrite": True
+    })
+    
+    transitions = [package_name for importer, package_name, _ in pkgutil.iter_modules(["slideshow/transitions"])]
+    
+    for transition in transitions:
+        input_files =   [{
+                            "file": "media\\andrey-andreyev-dh8ONmfQyQQ-unsplash.jpg",
+                            "transition": transition,
+                            "title": transition,
+                            "zoom_direction": "none",
+                            "scale_mode": "pad"
+                        },
+                        {
+                            "file": "media\\jeremy-bishop-h7bQ8VEZtws-unsplash.jpg",
+                            "title": transition,
+                            "zoom_direction": "none",
+                            "scale_mode": "pad"
+                        }]
+
+        sm = SlideManager(config, input_files, [])
+
+        sm.createVideo("demo/%s.gif" %(transition))
