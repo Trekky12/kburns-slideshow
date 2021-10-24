6d20bd6213ce6a93d55f44d9bd977bfe90391aca
Tobias <thaegi@gmx.de>
2021-10-24 10:13:00 +0200

    Fix delete of temporary files

--- slideshow/Queue.py
+++ slideshow/Queue.py
@@ -83,10 +83,10 @@ class Queue:
         return None
 
     def clean(self, delete_temp=True):
-        self.init()
         if delete_temp:
             for temp in self.tempFiles:
                 file = os.path.join(self.tempFileFolder, temp)
                 os.remove(file)
                 logger.debug("Delete %s", file)
             # os.rmdir(self.tempFileFolder)
+        self.init()
